bc0494e5fb364dfc265146d9d9b069dce013afbc
HITEN-MAHESHWARI <hitenkalani50@gmail.com>
2026-02-07 02:06:19 +0500

    Update intro.c
    Used Escape Sequence

--- intro.c
+++ intro.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  printf("Welcome to Programming Fundamental\n");
-  printf("Spring 2026\n");
+  printf("Name:\tHiten Kalani\n");
+  printf("Roll no:\t25k-6549\n");
+  printf("Section:\tBSCE-2B");
   return 0;
 }
